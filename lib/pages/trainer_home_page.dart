d4eb6a5168221f36caf0cfaeb355c153263fae85
Samuel Yuan <samuel6161@gmail.com>
2020-06-14 18:38:31 -0500

    added OneOnOneSessions

--- lib/pages/trainer_home_page.dart
+++ lib/pages/trainer_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fitnessmarketplace/models/OneOnOneSession.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/intl.dart';
@@ -13,13 +14,12 @@ class TrainerHomePage extends StatefulWidget {
 }
 
 class _TrainerHomePageState extends State<TrainerHomePage> {
-  List<String> names = ['Johnny Appleseed', 'Samuel Sam', 'John Smith'];
   List<String> dates = ['Jun 15, 5:00 pm', 'July 4, 9:00 pm', 'December 25, 12:00 am'];
-  List<String> duration = ['45 minutes', '1 hour', '24 hours'];
 
   CalendarController _calendarController;
   Trainer currentTrainer;
   List<RecordedVideo> trainerVideos;
+  List<OneOnOneSession>  oneOnOneSessions;
 
   @override
   void initState(){
@@ -36,10 +36,15 @@ class _TrainerHomePageState extends State<TrainerHomePage> {
 
   setUp() async {
     trainerVideos = new List<RecordedVideo>();
+    oneOnOneSessions = new List<OneOnOneSession>();
+
     FirebaseUser getUser = await FirebaseAuth.instance.currentUser();
     DocumentSnapshot userData = await Firestore.instance.collection('trainers').document(getUser.uid).get();
     currentTrainer = Trainer.fromSnapshot(userData);
+
+    await getOneOnOneSessions();
     await getVideos();
+
     setState(() {
       
     });
@@ -49,8 +54,15 @@ class _TrainerHomePageState extends State<TrainerHomePage> {
     QuerySnapshot getVideos = await currentTrainer.reference.collection('recordedVideos').getDocuments();
     List<DocumentSnapshot> allVideos = getVideos.documents;
     for (int i = 0; i < allVideos.length; i++) {
-      print('got');
       trainerVideos.add(RecordedVideo.fromSnapshot(allVideos[i]));
+    }
+  }
+  
+  getOneOnOneSessions() async {
+    QuerySnapshot getOneOnOneSessions = await currentTrainer.reference.collection('oneOnOneSessions').getDocuments();
+    List<DocumentSnapshot> allOneOnOneSessions = getOneOnOneSessions.documents;
+    for (int i = 0; i < allOneOnOneSessions.length; i++) {
+      oneOnOneSessions.add(OneOnOneSession.fromSnapshot(allOneOnOneSessions[i]));
     }
   }
 
@@ -87,8 +99,12 @@ class _TrainerHomePageState extends State<TrainerHomePage> {
               Container(
                 height: 300.0,
                 child: ListView.builder(
-                  itemCount: names.length,
-                  itemBuilder: (BuildContext context, int index){
+                  itemCount: oneOnOneSessions.length,
+                  itemBuilder: (BuildContext context, int i) {
+
+                    DateTime oneOnOneSessionDate = oneOnOneSessions[i].date.toDate().toLocal();
+                    String oneOnOneSessionLength = getLengthFromInt(oneOnOneSessions[i].length);
+
                     return Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: Container(
@@ -114,18 +130,18 @@ class _TrainerHomePageState extends State<TrainerHomePage> {
                               mainAxisAlignment: MainAxisAlignment.center,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(names[index], style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600, color: Colors.white),),
+                                Text(oneOnOneSessions[i].name, style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600, color: Colors.white),),
                                 SizedBox(height: 10.0,),
-                                Text(duration[index], style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w400, color: Colors.white),),
+                                Text(oneOnOneSessionLength, style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w400, color: Colors.white),),
                               ],
                             ),
                             Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               crossAxisAlignment: CrossAxisAlignment.end,
                               children: [
-                                Text(dates[index].substring(0, dates[index].indexOf(',')), style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400, color: Colors.white),),
+                                Text(oneOnOneSessionDate.month.toString() + '/' + oneOnOneSessionDate.day.toString(), style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400, color: Colors.white),),
                                 SizedBox(height: 10.0,),
-                                Text(dates[index].substring(dates[index].indexOf(',') + 2), style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400, color: Colors.white),),
+                                Text(oneOnOneSessionDate.hour.toString() + ':' + oneOnOneSessionDate.minute.toString(), style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400, color: Colors.white),),
                               ],
                             )
                           ],
@@ -268,6 +284,15 @@ class _TrainerHomePageState extends State<TrainerHomePage> {
         ),
       ),
     );
+  }
+
+  String getLengthFromInt(int length) {
+    String returnLength = '';
+    if (length > 60) {
+      returnLength = returnLength + (length ~/ 60).toString() + ' hours ';
+    }
+    returnLength = returnLength + (length % 60).toString() + ' minutes';
+    return returnLength;
   }
 
 }
