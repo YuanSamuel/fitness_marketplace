8e0e7303ec5206c119cac0abf5ba0f2404df90ac
micha <michael@mohn.com>
2020-06-13 21:07:26 -0500

    Onboarding without the pictures in and words from chatterbot

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:fitnessmarketplace/pages/agora_test_index_page.dart';
 import 'package:flutter/material.dart';
 import 'pages/profile_page.dart';
+import 'pages/intro_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: StudentProfilePage(4,5,3,"Michael Mohn"),
+      home: IntroScreen(),
     );
   }
 }
