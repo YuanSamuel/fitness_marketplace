67625a06dd2c93d4eb4b3ed848206f611b50331a
Samuel Yuan <samuel6161@gmail.com>
2020-06-14 18:42:17 -0500

    roll back pull

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,10 @@
-
 import 'package:fitnessmarketplace/pages/register_page.dart';
+import 'package:fitnessmarketplace/pages/user_home_page.dart';
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 import 'register_page.dart';
-import 'package:fitnessmarketplace/pages/trainer_home_screen.dart';
+import 'package:fitnessmarketplace/pages/trainer_home_page.dart';
 
 class Login extends StatefulWidget {
 ***REMOVED***
@@ -14,7 +14,6 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   TextEditingController _email = new TextEditingController();
   TextEditingController _password = new TextEditingController();
-  bool _isTrainer = true;
 
 ***REMOVED***
 ***REMOVED***
@@ -37,15 +36,23 @@ class _LoginState extends State<Login> {
               FirebaseAuth.instance
                   .signInWithEmailAndPassword(
                       email: _email.text, password: _password.text)
-                  .then((currentUser) {
-                    if (_isTrainer) {
+                  .then((currentUser) async {
+                    print(currentUser.user.uid);
+                    DocumentSnapshot snapshot = await Firestore.instance.collection('users').document(currentUser.user.uid).get();
+                    print(snapshot.data);
+                    if (snapshot.data['isTrainer']) {
 ***REMOVED***
                         context,
-                        MaterialPageRoute(builder: (context) => TrainerHomeScreen()),
+                        MaterialPageRoute(builder: (context) => TrainerHomePage()),
+                  ***REMOVED***
+                    ***REMOVED***
+                ***REMOVED***
+***REMOVED***
+                        context,
+                        MaterialPageRoute(builder: (context) => UserHomePage()),
                   ***REMOVED***
                     ***REMOVED***
               ***REMOVED***);
-              print('Logged in ' + _email.text + ' as User ID ' + userid);
             ***REMOVED***,
 ***REMOVED***
           FlatButton(
