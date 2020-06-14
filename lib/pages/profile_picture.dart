4745f7d87b04c1bc268fd717ff637933df49f1e5
Ian <enactirace@gmail.com>
2020-06-13 20:19:56 -0500

    e

--- lib/pages/profile_picture.dart
+++ lib/pages/profile_picture.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
 ***REMOVED***
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+***REMOVED***
+import 'register.dart';
 
 class ProfilePic extends StatefulWidget {
 ***REMOVED***
@@ -6,10 +12,56 @@ class ProfilePic extends StatefulWidget {
 ***REMOVED***
 
 class _ProfilePicState extends State<ProfilePic> {
+
+  final _picker = new ImagePicker();
+  String url;
+
+  Upload(File image) async {
+    FirebaseStorage _storage = FirebaseStorage();
+    String filePath = 'images/${DateTime.now()***REMOVED***.png';
+    StorageUploadTask _uploadTask = _storage.ref().child(filePath).putFile(image);
+    await _uploadTask.onComplete;
+
+    url = await _storage.ref().child(filePath).getDownloadURL();
+    Firestore.instance.collection('users').document(userid).setData({
+      'url':url,
+    ***REMOVED***,merge: true);
+  ***REMOVED***
+
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-      floatingActionButton: FloatingActionButton(onPressed: ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add_a_photo),
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (_) => Scaffold(
+                body: SizedBox(
+                  width: MediaQuery.of(context).size.width-10,
+        ***REMOVED***
+        ***REMOVED***
+                      FlatButton(
+    ***REMOVED***'From Camera'),
+                        onPressed: () async {
+                          PickedFile image = await _picker.getImage(source: ImageSource.camera);
+                          Upload(File(image.path));
+                        ***REMOVED***,
+        ***REMOVED***,
+                      FlatButton(
+    ***REMOVED***'From Device'),
+                        onPressed: () async {
+                          PickedFile image = await _picker.getImage(source: ImageSource.gallery);
+                            Upload(File(image.path));
+                        ***REMOVED***,
+        ***REMOVED***,
+    ***REMOVED***
+    ***REMOVED***
+  ***REMOVED***
+***REMOVED***
+      ***REMOVED***
+        ***REMOVED***
+***REMOVED***
 ***REMOVED***
   ***REMOVED***
 ***REMOVED***
