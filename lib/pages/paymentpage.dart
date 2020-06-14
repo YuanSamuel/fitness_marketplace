7b7bc9e0117aafe1e976e57d814a5cb88e3bd4f0
abdur_100 <abdur.aziz10@gmail.com>
2020-06-14 15:37:46 -0500

    PaymentPage

--- lib/pages/paymentpage.dart
+++ lib/pages/paymentpage.dart
@@ -2,6 +2,7 @@ import 'package:fitnessmarketplace/pages/show_video_page.dart';
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
 
 
  class PaymentPage extends StatefulWidget{
@@ -12,7 +13,7 @@ import 'package:fitnessmarketplace/pages/show_video_page.dart';
 
  class _PaymentPageState extends State<PaymentPage>{
 
-   void_pay(){
+ ***REMOVED***
  ***REMOVED***
  ***REMOVED***
  ***REMOVED***
@@ -40,8 +41,29 @@ import 'package:fitnessmarketplace/pages/show_video_page.dart';
 
 ***REMOVED***
 ***REMOVED***
-    // TODO: implement build
-    throw UnimplementedError();
+***REMOVED***
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage("https://www.paymentsjournal.com/wp-content/uploads/2017/05/Fotolia_90732811_Subscription_Monthly_M.jpg"),
+            fit: BoxFit.cover,
+          )
+***REMOVED***
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Please Proceed to make your payment'
+            )
+          ],
+***REMOVED***
+***REMOVED***
+          floatingActionButton: FloatingActionButton(
+          onPressed: _pay,
+          tooltip: 'Enter Card Number',
+          child: Icon(Icons.payment),
+***REMOVED***
+***REMOVED***
   ***REMOVED***
 
 
