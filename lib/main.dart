8a981a13ff00b9561c4f06e40c18747d6a7953ae
Samuel Yuan <samuel6161@gmail.com>
2020-06-14 15:25:41 -0500

    fix error

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       debugShowCheckedModeBanner: false,
-      home: Register(),
+      home: TrainerHomePage(),
     );
   }
 }
