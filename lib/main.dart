4539ffae6b240939ecbcb7f204b194914a685f66
VincentDo8439 <33644870+VincentDo8439@users.noreply.github.com>
2020-06-14 18:10:37 -0500

    added user marketplace and trainer page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:fitnessmarketplace/pages/trainer_home_screen.dart';
+import 'package:fitnessmarketplace/pages/user_marketplace_screen.dart';
 ***REMOVED***
 import 'package:fitnessmarketplace/pages/register_page.dart';
 import 'package:fitnessmarketplace/pages/trainer_home_page.dart';
-import 'package:fitnessmarketplace/pages/register.dart';
 
 ***REMOVED***
 ***REMOVED***
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
 ***REMOVED***
       debugShowCheckedModeBanner: false,
-      home: Register(),
+      home: MarketplaceScreen(),
 ***REMOVED***
   ***REMOVED***
 ***REMOVED***
