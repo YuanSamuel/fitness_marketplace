2bf2f6b3f8345ee6a5a00c901e5e136c0ba002ef
Ian <enactirace@gmail.com>
2020-06-13 19:15:57 -0500

    e

---
+++ lib/pages/profile_picture.dart
@@ -0,0 +1,15 @@
+***REMOVED***
+
+class ProfilePic extends StatefulWidget {
+***REMOVED***
+  _ProfilePicState createState() => _ProfilePicState();
+***REMOVED***
+
+class _ProfilePicState extends State<ProfilePic> {
+***REMOVED***
+***REMOVED***
+***REMOVED***
+      floatingActionButton: FloatingActionButton(onPressed: ),
+***REMOVED***
+  ***REMOVED***
+***REMOVED***
