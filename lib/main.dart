2e64a7a628bd366af53792e9f5a2f1d319054d5b
lucaszcai <lucas.z.cai@gmail.com>
2020-06-15 10:39:50 -0500

    polished the trainer ui a bit, added the student UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fitnessmarketplace/pages/student_home_screen.dart';
 import 'package:fitnessmarketplace/pages/trainer_home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:fitnessmarketplace/pages/trainer_home_page.dart';
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       debugShowCheckedModeBanner: false,
-      home: TrainerHomeScreen(),
+      home: StudentHomePage(),
     );
   }
 }
