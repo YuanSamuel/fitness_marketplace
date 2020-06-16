a7892fff80bdc99132b49e25fa249840d54f13f1
Samuel Yuan <samuel6161@gmail.com>
2020-06-15 22:26:56 -0500

    student model, updated student home page

--- lib/models/OneOnOneSession.dart
+++ lib/models/OneOnOneSession.dart
@@ -3,12 +3,14 @@
 class OneOnOneSession {
   String name;
   String studentUid;
+  String trainerName;
+  String photoUrl;
   int length;
   Timestamp date;
 
   DocumentReference reference;
 
-  OneOnOneSession({this.name, this.studentUid, this.length, this.date, this.reference***REMOVED***);
+  OneOnOneSession({this.name, this.studentUid, this.trainerName, this.photoUrl, this.length, this.date, this.reference***REMOVED***);
 
   factory OneOnOneSession.fromSnapshot(DocumentSnapshot snapshot) {
     OneOnOneSession newOneOnOneSession = OneOnOneSession.fromJson(snapshot.data);
@@ -20,6 +22,8 @@ class OneOnOneSession {
     return OneOnOneSession(
       name: json['name'] as String,
       studentUid: json['studentUid'] as String,
+      trainerName: json['trainerName'] as String,
+      photoUrl: json['photoUrl'] as String,
       length: json['length'] as int,
       date: json['date'] as Timestamp,
 ***REMOVED***
@@ -31,6 +35,8 @@ class OneOnOneSession {
     return <String, dynamic>{
       'name': instance.name,
       'studentUid': instance.studentUid,
+      'trainerName': instance.trainerName,
+      'photoUrl': instance.photoUrl,
       'length': instance.length,
       'date': instance.date
     ***REMOVED***;
--- lib/pages/user_home_page.dart
+++ lib/pages/user_home_page.dart
@@ -1,6 +1,12 @@
 ***REMOVED***
+***REMOVED***
+import 'package:fitnessmarketplace/models/OneOnOneSession.dart';
+import 'package:fitnessmarketplace/models/RecordedVideo.dart';
+import 'package:fitnessmarketplace/pages/show_video_page.dart';
+***REMOVED***
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/intl.dart';
+import 'package:fitnessmarketplace/models/Student.dart';
 
 class UserHomePage extends StatefulWidget {
 ***REMOVED***
@@ -8,16 +14,19 @@ class UserHomePage extends StatefulWidget {
 ***REMOVED***
 
 class _UserHomePageState extends State<UserHomePage> {
-  List<String> trainerNames = ['Johnny Appleseed', 'Samuel Sam', 'John Smith'];
-  List<String> sessionNames = ['Yoga', 'Karate', 'HIIT'];
-  List<String> dates = ['Jun 15, 5:00 pm', 'July 4, 9:00 pm', 'December 25, 12:00 am'];
-  List<String> duration = ['45 minutes', '1 hour', '24 hours'];
+  List<String> months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+  Student currentStudent;
+  List<OneOnOneSession> oneOnOneSessions;
+  List<RecordedVideo> recordedVideos;
 
   CalendarController _calendarController;
 
 ***REMOVED***
-  void initState(){
+***REMOVED***
     _calendarController = CalendarController();
+    getCurrentStudent();
+***REMOVED***
   ***REMOVED***
 
 ***REMOVED***
@@ -26,167 +35,266 @@ class _UserHomePageState extends State<UserHomePage> {
     super.dispose();
   ***REMOVED***
 
+  void getCurrentStudent() async {
+    FirebaseUser getUser = await FirebaseAuth.instance.currentUser();
+    DocumentSnapshot userData = await Firestore.instance
+        .collection('students')
+        .document(getUser.uid)
+        .get();
+    currentStudent = Student.fromSnapshot(userData);
+    await getOneOnOneSessions();
+    await getRecordedVideos();
+    setState(() {***REMOVED***);
+  ***REMOVED***
+
+  Future<void> getOneOnOneSessions() async {
+    oneOnOneSessions = new List<OneOnOneSession>();
+    QuerySnapshot allOneOnOneSessions = await currentStudent.reference.collection('oneOnOneSessions').getDocuments();
+    List<DocumentSnapshot> holdOneOnOneSessions = allOneOnOneSessions.documents;
+    for (int i = 0; i < holdOneOnOneSessions.length; i++) {
+      OneOnOneSession currentOneOnOneSession = OneOnOneSession.fromSnapshot(holdOneOnOneSessions[i]);
+      oneOnOneSessions.add(currentOneOnOneSession);
+    ***REMOVED***
+    return;
+  ***REMOVED***
+
+  Future<void> getRecordedVideos() async {
+    recordedVideos = new List<RecordedVideo>();
+    QuerySnapshot allRecordedVideos = await currentStudent.reference.collection('recordedVideos').getDocuments();
+    List<DocumentSnapshot> videos = allRecordedVideos.documents;
+    for (int i = 0; i < videos.length; i++) {
+      RecordedVideo currentVideo = RecordedVideo.fromSnapshot(videos[i]);
+      recordedVideos.add(currentVideo);
+    ***REMOVED***
+    return;
+  ***REMOVED***
 
 ***REMOVED***
 ***REMOVED***
+    if (currentStudent == null) {
+  ***REMOVED***
+        body: Center(
+          child: CircularProgressIndicator(),
 ***REMOVED***
-      body:
-      SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 30.0,),
-            Padding(
+  ***REMOVED***
+    ***REMOVED*** else {
+  ***REMOVED***
+  ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+                height: 30.0,
+***REMOVED***,
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Row(
+        ***REMOVED***
+***REMOVED***
+                        'Student: ' + currentStudent.firstName,
+    ***REMOVED***
+                            fontSize: 30.0, fontWeight: FontWeight.w600),
+        ***REMOVED***,
+                      Spacer(),
+                      IconButton(
+                        icon: Icon(Icons.account_circle),
+    ***REMOVED******REMOVED***,
+        ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***),
+              Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                child: Row(
-      ***REMOVED***
-                    Text('Student: Lucas', style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w600),),
-                    Spacer(),
-                    IconButton(
-                      icon: Icon(Icons.account_circle),
-                      onPressed: (){***REMOVED***,
-      ***REMOVED***
-  ***REMOVED***
-  ***REMOVED***
-***REMOVED***
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15.0),
-              child: Divider(height: 10.0, thickness: 0.75,),
-***REMOVED***
-            Container(child: _buildCalendar()),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15.0),
-              child: Divider(height: 10.0, thickness: 0.75,),
-***REMOVED***
-            Padding(
-              padding: const EdgeInsets.only(left: 15.0),
-              child: Text('Upcoming Sessions', style: TextStyle(
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.bold
+                child: Divider(
+                  height: 10.0,
+                  thickness: 0.75,
+  ***REMOVED***,
 ***REMOVED***,
+              Container(child: _buildCalendar()),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Divider(
+                  height: 10.0,
+                  thickness: 0.75,
+  ***REMOVED***,
 ***REMOVED***,
-***REMOVED***
-            Container(
-              height: 300.0,
-              child: ListView.builder(
-                itemCount: trainerNames.length,
-                itemBuilder: (BuildContext context, int index){
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 15.0),
-          ***REMOVED***
-                        borderRadius: BorderRadius.circular(30.0),
-                        color: Color(0xff3B3B3B),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: Offset(0, 3), // changes position of shadow
-            ***REMOVED***,
-        ***REMOVED***
-        ***REMOVED***,
-                      height: 100.0,
-                      child: Row(
-        ***REMOVED***
-                        crossAxisAlignment: CrossAxisAlignment.center,
-            ***REMOVED***
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                ***REMOVED***
-                ***REMOVED***
-        ***REMOVED***sessionNames[index] + " | " + trainerNames[index], style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600, color: Colors.white),),
-        ***REMOVED***height: 10.0,),
-        ***REMOVED***duration[index], style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w400, color: Colors.white),),
-            ***REMOVED***
-            ***REMOVED***,
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                ***REMOVED***
-        ***REMOVED***dates[index].substring(0, dates[index].indexOf(',')), style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400, color: Colors.white),),
-        ***REMOVED***height: 10.0,),
-        ***REMOVED***dates[index].substring(dates[index].indexOf(',') + 2), style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400, color: Colors.white),),
-            ***REMOVED***
-            ***REMOVED***
-        ***REMOVED***
-        ***REMOVED***,
-      ***REMOVED***,
-              ***REMOVED***
-                ***REMOVED***,
+              Padding(
+                padding: const EdgeInsets.only(left: 15.0),
+                child: Text(
+                  'Upcoming Sessions',
+***REMOVED***fontSize: 25.0, fontWeight: FontWeight.bold),
+  ***REMOVED***,
 ***REMOVED***,
-***REMOVED***
+              Container(
+                height: 250.0,
+                child: ListView.builder(
+                  itemCount: oneOnOneSessions.length,
+                  itemBuilder: (BuildContext context, int i) {
+                    DateTime oneOnOneSessionDate = oneOnOneSessions[i].date.toDate();
+                    DateTime localOneOnOneSessionDate = oneOnOneSessionDate.toLocal();
+                    String oneOnOneSessionDateString = getDateStringFromDateTime(localOneOnOneSessionDate);
+                    String oneOnOneSessionTimeString = getTimeStringFromDateTime(localOneOnOneSessionDate);
+                    String oneOnOneSessionLengthString = getLengthFromInt(oneOnOneSessions[i].length);
 
-            SizedBox(height: 10.0,),
-            Container(
-  ***REMOVED***
-                  color: Colors.white,
-                  border: Border.symmetric(
-                    vertical: BorderSide(
-                        width: 0.5,
-                        color: Colors.black26
-      ***REMOVED***,
-    ***REMOVED***
-***REMOVED***,
-    ***REMOVED***
-    ***REMOVED***
-    ***REMOVED***
-    ***REMOVED***height: 10.0,),
-                  Padding(
-                      padding: const EdgeInsets.symmetric(horizontal:15.0),
-                      child:Row(
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(horizontal: 15.0),
             ***REMOVED***
-    ***REMOVED***'Your Videos',  style: TextStyle(
-                              fontSize: 25.0,
-        ***REMOVED***
-            ***REMOVED***,
-            ***REMOVED***,
-                          Spacer(),
-    ***REMOVED***'See All', style: TextStyle(
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black26
-            ***REMOVED***,),
-        ***REMOVED***
-        ***REMOVED***
-    ***REMOVED***,
-                  Container(
-                    color: Colors.white,
-                    height: 300.0,
-                    child: ListView.builder(
-                      itemCount: 3,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (BuildContext context, int index){
-                        return Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Container(
-                            height: 300.0,
-                            width: 300.0,
-                ***REMOVED***
-                              borderRadius: BorderRadius.circular(30.0),
-                              color: Colors.blue,
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  spreadRadius: 5,
-                                  blurRadius: 7,
-                                  offset: Offset(0, 3), // changes position of shadow
+                          borderRadius: BorderRadius.circular(30.0),
+                          color: Color(0xff3B3B3B),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset:
+                                  Offset(0, 3), // changes position of shadow
+              ***REMOVED***,
+          ***REMOVED***
+          ***REMOVED***,
+                        height: 100.0,
+                        child: Row(
+          ***REMOVED***
+                          crossAxisAlignment: CrossAxisAlignment.center,
+              ***REMOVED***
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                  ***REMOVED***
+                  ***REMOVED***
+          ***REMOVED***
+                                  oneOnOneSessions[i].name +
+                                      " | " +
+                                      oneOnOneSessions[i].trainerName,
+              ***REMOVED***
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.white),
+                  ***REMOVED***,
+          ***REMOVED***
+                                  height: 10.0,
+                  ***REMOVED***,
+          ***REMOVED***
+                                  oneOnOneSessionLengthString,
+              ***REMOVED***
+                                      fontSize: 15.0,
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.white),
                   ***REMOVED***,
               ***REMOVED***
               ***REMOVED***,
-            ***REMOVED***,
-                    ***REMOVED***
-                      ***REMOVED***,
-      ***REMOVED***,
-    ***REMOVED***,
-***REMOVED***
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                  ***REMOVED***
+          ***REMOVED***
+                                  oneOnOneSessionDateString,
+              ***REMOVED***
+                                      fontSize: 12.0,
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.white),
+                  ***REMOVED***,
+          ***REMOVED***
+                                  height: 10.0,
+                  ***REMOVED***,
+          ***REMOVED***
+                                  oneOnOneSessionTimeString,
+              ***REMOVED***
+                                      fontSize: 12.0,
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.white),
+                  ***REMOVED***,
+              ***REMOVED***
+              ***REMOVED***
+          ***REMOVED***
+          ***REMOVED***,
+        ***REMOVED***,
+                ***REMOVED***
+                  ***REMOVED***,
+  ***REMOVED***,
 ***REMOVED***,
-            )
-          ],
+***REMOVED***
+                height: 10.0,
+***REMOVED***,
+              Container(
+    ***REMOVED***
+                    color: Colors.white,
+                    border: Border.symmetric(
+                      vertical: BorderSide(width: 0.5, color: Colors.black26),
+      ***REMOVED***),
+      ***REMOVED***
+      ***REMOVED***
+      ***REMOVED***
+      ***REMOVED***
+                      height: 10.0,
+      ***REMOVED***,
+                    Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                        child: Row(
+              ***REMOVED***
+      ***REMOVED***
+                              'Your Videos',
+          ***REMOVED***
+                                  fontSize: 25.0, fontWeight: FontWeight.bold),
+              ***REMOVED***,
+                            Spacer(),
+      ***REMOVED***
+                              'See All',
+          ***REMOVED***
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.black26),
+              ***REMOVED***,
+          ***REMOVED***
+          ***REMOVED***),
+                    Container(
+                      color: Colors.white,
+                      height: 300.0,
+                      child: ListView.builder(
+                        itemCount: recordedVideos.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (BuildContext context, int i) {
+                          return Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: GestureDetector(
+                              child: Container(
+                                  height: 300.0,
+                                  width: 300.0,
+                      ***REMOVED***
+                                    borderRadius: BorderRadius.circular(30.0),
+                                    color: Colors.blue,
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.grey.withOpacity(0.5),
+                                        spreadRadius: 5,
+                                        blurRadius: 7,
+                                        offset: Offset(
+                                            0, 3), // changes position of shadow
+                        ***REMOVED***,
+                    ***REMOVED***
+                    ***REMOVED***,
+                                  child: Center(
+                ***REMOVED***recordedVideos[i].name + ' ' + recordedVideos[i].date.toDate().toString()),
+                    ***REMOVED***
+                ***REMOVED***,
+                              onTap: () {
+          ***REMOVED***
+        ***REMOVED***
+        ***REMOVED***builder: (context) => ShowVideoPage(videoDownloadUrl: recordedVideos[i].videoUrl,)),
+                            ***REMOVED***
+                              ***REMOVED***,
+              ***REMOVED***,
+                      ***REMOVED***
+                        ***REMOVED***,
+        ***REMOVED***,
+      ***REMOVED***,
+  ***REMOVED***
+  ***REMOVED***,
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+  ***REMOVED***
+    ***REMOVED***
   ***REMOVED***
 
   Widget _buildCalendar() {
@@ -264,5 +372,30 @@ class _UserHomePageState extends State<UserHomePage> {
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+  ***REMOVED***
+
+  getDateStringFromDateTime(DateTime time) {
+    return months[time.month] + ' ' + time.day.toString();
+  ***REMOVED***
+
+  getTimeStringFromDateTime(DateTime time) {
+    if (time.hour > 12) {
+      return (time.hour - 12).toString() +  ':' + time.minute.toString() + ' p.m.';
+    ***REMOVED***
+***REMOVED***
+      if (time.hour == 0) {
+        return '12:' + time.minute.toString() + 'a.m.';
+      ***REMOVED***
+      return time.hour.toString() + ':' + time.minute.toString() + ' a.m.';
+    ***REMOVED***
+  ***REMOVED***
+
+  getLengthFromInt(int length) {
+    String duration = '';
+    if (length > 60) {
+      duration = duration + (length ~/ 60).toString() + ' hours ';
+    ***REMOVED***
+    duration = duration + (length % 60).toString() + ' minutes';
+    return duration;
   ***REMOVED***
 ***REMOVED***
