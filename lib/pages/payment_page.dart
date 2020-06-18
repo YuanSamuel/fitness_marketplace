8a8ea1b7e849c2d785a93a4cecede6f26f4826f9
PrasannS <singhal.prasann07@gmail.com>
2020-06-18 17:40:50 -0500

    merged w lucas' code and fixed pricing code

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:square_in_app_payments/models.dart';
 import 'package:square_in_app_payments/in_app_payments.dart';
 
