fb70743654f5457bd5031fdf7ffb534a4ced92f2
VincentDo8439 <33644870+VincentDo8439@users.noreply.github.com>
2020-06-18 14:24:45 -0500

    added user and trainer navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitnessmarketplace/pages/register_page.dart';
 import 'package:fitnessmarketplace/pages/root_page.dart';
+import 'package:fitnessmarketplace/pages/trainer_navigation.dart';
+import 'package:fitnessmarketplace/pages/user_navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:fitnessmarketplace/pages/trainer_home_page.dart';
 
