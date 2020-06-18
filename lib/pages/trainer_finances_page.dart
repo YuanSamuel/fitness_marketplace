a0f9d4779e516d023f1d067ae456ea9194f8bd57
Samuel Yuan <samuel6161@gmail.com>
2020-06-18 17:36:00 -0500

    hold data

---
+++ lib/pages/trainer_finances_page.dart
@@ -0,0 +1,39 @@
+***REMOVED***
+***REMOVED***
+import 'package:fitnessmarketplace/models/PrivateSession.dart';
+import 'package:fitnessmarketplace/models/RecordedVideo.dart';
+***REMOVED***
+***REMOVED***
+
+class TrainerFinancesPage extends StatefulWidget {
+  TrainerFinancesPage({Key key***REMOVED***) : super(key: key);
+
+***REMOVED***
+  _TrainerFinancesPageState createState() => _TrainerFinancesPageState();
+***REMOVED***
+
+class _TrainerFinancesPageState extends State<TrainerFinancesPage> {
+
+  Trainer currentTrainer;
+  List<RecordedVideo> trainerVideos;
+  List<PrivateSession> trainerPrivateSessions;
+  
+
+***REMOVED***
+***REMOVED***
+    getUser();
+***REMOVED***
+  ***REMOVED***
+
+  getUser() async {
+    FirebaseUser getUser = await FirebaseAuth.instance.currentUser();
+    DocumentSnapshot userData = await Firestore.instance.collection('trainers').document(getUser.uid).get();
+    currentTrainer = Trainer.fromSnapshot(userData);
+  ***REMOVED***
+
+***REMOVED***
+***REMOVED***
+    // TODO: implement build
+    throw UnimplementedError();
+  ***REMOVED***
+***REMOVED***
