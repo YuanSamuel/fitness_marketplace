752f0336dec1db3a6ea81b476177765e0591f5d5
abdur100 <50559372+abdur100@users.noreply.github.com>
2020-06-18 17:14:56 -0500

    Update payment_page.dart

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -37,10 +37,33 @@
 
    ***REMOVED***
 
+  ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-    // TODO: implement build
-    throw UnimplementedError();
+      appBar: AppBar(
+        title: Text('Proceed to Payment',
+        style: TextStyle(
+          fontStyle: FontStyle.italic
+ ***REMOVED***
+        )
+***REMOVED***
+      body : Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage('https://www.debt.com/wp-content/uploads/2014/05/Credit-Card-Visa-Master-Card-Background.jpg'),
+            fit: BoxFit.cover,
+            alignment: Alignment.topCenter
+          )
+***REMOVED***
+
+***REMOVED***
+      floatingActionButton: FloatingActionButton(
+        onPressed:_pay,
+        tooltip: 'Enter Card Number',
+        child: Icon(Icons.payment),
+***REMOVED***
+
+***REMOVED***
   ***REMOVED***
 
 
