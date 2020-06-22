0ccae21fac77c72d75d1dc859607da84f006f286
Samuel Yuan <samuel6161@gmail.com>
2020-06-21 21:56:28 -0500

    bugfixes

--- lib/pages/request_private_session_page.dart
+++ lib/pages/request_private_session_page.dart
@@ -39,7 +39,7 @@ class _RequestPrivateSessionPageState extends State<RequestPrivateSessionPage> {
     _calendarController = new CalendarController();
     _calendarHelper = new CalendarHelper();
     _stringHelper = new StringHelper();
-    events = new List<PrivateSession>();
+    events = new List<dynamic>();
     setUp();
     super.initState();
   }
@@ -147,7 +147,6 @@ class _RequestPrivateSessionPageState extends State<RequestPrivateSessionPage> {
                                 builder: (context) => SessionPreview(
                                       stream: events[i],
                                       isStream: true,
-                                      trainer: widget.trainer,
                                     )),
                           );
                         } else {
