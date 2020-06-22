56467eb297759788c2637b96787dd7fdf5d689cb
Samuel Yuan <samuel6161@gmail.com>
2020-06-22 09:33:22 -0500

    more integration

--- lib/pages/user_navigation.dart
+++ lib/pages/user_navigation.dart
@@ -1,4 +1,7 @@
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+***REMOVED***
+***REMOVED***
+import 'package:fitnessmarketplace/models/Student.dart';
 import 'package:fitnessmarketplace/pages/user_home_page.dart';
 import 'package:fitnessmarketplace/pages/user_marketplace_page.dart';
 import 'package:fitnessmarketplace/pages/user_profile_page.dart';
@@ -10,18 +13,49 @@ class UserNavigation extends StatefulWidget {
 ***REMOVED***
 
 class _UserNavigationState extends State<UserNavigation> {
+
+  String name;
+  int numClasses;
+  int numPrivateSessions;
+  int numVideos;
+  bool doneLoading;
+
   int _currentIndex = 0;
   PageController _pageController = PageController(initialPage: 0);
-  var _pageOptions = [
-    UserHomePage(),
-    MarketplaceScreen(),
-    UserProfilePage(1, 2, 3, 'SamuelY'),
-  ];
+  var _pageOptions;
 
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
+    doneLoading = false;
     _pageController = PageController();
+    getData();
+***REMOVED***
+  ***REMOVED***
+
+  getData() async {
+    FirebaseUser currentUser = await FirebaseAuth.instance.currentUser();
+    DocumentSnapshot userData = await Firestore.instance.collection('students').document(currentUser.uid).get();
+    Student currentStudent =  Student.fromSnapshot(userData);
+    name = currentStudent.firstName + ' ' + currentStudent.lastName;
+
+    QuerySnapshot getClasses = await currentStudent.reference.collection('streams').getDocuments();
+    numClasses = getClasses.documents.length;
+
+    QuerySnapshot getPrivateSessions = await currentStudent.reference.collection('privateSessions').getDocuments();
+    numPrivateSessions = getPrivateSessions.documents.length;
+
+    QuerySnapshot getVideos = await currentStudent.reference.collection('videos').getDocuments();
+    numVideos = getVideos.documents.length;
+
+    _pageOptions = [
+    UserHomePage(),
+    MarketplaceScreen(),
+    UserProfilePage(numClasses, numPrivateSessions, numVideos, name),
+    ];
+
+    setState(() {
+      doneLoading = true;
+    ***REMOVED***);
   ***REMOVED***
 
 ***REMOVED***
@@ -32,43 +66,52 @@ class _UserNavigationState extends State<UserNavigation> {
 
 ***REMOVED***
 ***REMOVED***
+    if (!doneLoading) {
+  ***REMOVED***
+        body: Center(
+          child: CircularProgressIndicator(),
 ***REMOVED***
-        body: SizedBox.expand(
-          child: PageView(
-            controller: _pageController,
-            onPageChanged: (index) {
-        ***REMOVED***
-                _currentIndex = index;
-              ***REMOVED***);
-            ***REMOVED***,
-            children: _pageOptions,
+  ***REMOVED***
+    ***REMOVED***
+***REMOVED***
+  ***REMOVED***
+          body: SizedBox.expand(
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+          ***REMOVED***
+                  _currentIndex = index;
+                ***REMOVED***);
+              ***REMOVED***,
+              children: _pageOptions,
 ***REMOVED***
 ***REMOVED***
-        bottomNavigationBar: BottomNavyBar(
-          selectedIndex: _currentIndex,
-          showElevation: true, // use this to remove appBar's elevation
-          itemCornerRadius: 20,
-          curve: Curves.easeInBack,
-          onItemSelected: (index) => setState(() {
-            _currentIndex = index;
-            _pageController.jumpToPage(index);
-          ***REMOVED***),
-          items: [
-            BottomNavyBarItem(
-              icon: Icon(Icons.home),
-              title: Text('Home'),
-              activeColor: Colors.red,
+          bottomNavigationBar: BottomNavyBar(
+            selectedIndex: _currentIndex,
+            showElevation: true, // use this to remove appBar's elevation
+            itemCornerRadius: 20,
+            curve: Curves.easeInBack,
+            onItemSelected: (index) => setState(() {
+              _currentIndex = index;
+              _pageController.jumpToPage(index);
+            ***REMOVED***),
+            items: [
+              BottomNavyBarItem(
+                icon: Icon(Icons.home),
+                title: Text('Home'),
+                activeColor: Colors.red,
+***REMOVED***,
+              BottomNavyBarItem(
+                  icon: Icon(Icons.shopping_cart),
+                  title: Text('Marketplace'),
+                  activeColor: Colors.purpleAccent),
+              BottomNavyBarItem(
+                icon: Icon(Icons.account_circle),
+                title: Text('Profile'),
+                activeColor: Colors.blue,
 ***REMOVED***
-            BottomNavyBarItem(
-                icon: Icon(Icons.shopping_cart),
-                title: Text('Marketplace'),
-                activeColor: Colors.purpleAccent),
-            BottomNavyBarItem(
-              icon: Icon(Icons.account_circle),
-              title: Text('Profile'),
-              activeColor: Colors.blue,
-            )
-          ],
-        ));
+***REMOVED***
+          ));
+    ***REMOVED***
   ***REMOVED***
 ***REMOVED***
--- lib/pages/user_profile_page.dart
+++ lib/pages/user_profile_page.dart
@@ -5,15 +5,15 @@
 //The profile system is set up so when you open the page you call either Driver or
 
 class UserProfilePage extends StatelessWidget {
-  int customers;
-  int rating;
-  int years;
+  int numClasses;
+  int numPrivateSessions;
+  int numVideos;
   String name;
 
-  UserProfilePage(int cust, int rat, int year, String n) {
-    customers = cust;
-    rating = rat;
-    years = year;
+  UserProfilePage(int classes, int privateSessions, int videos, String n) {
+    numClasses = classes;
+    numPrivateSessions = privateSessions;
+    numVideos = videos;
     name = n;
   ***REMOVED***
 
@@ -21,140 +21,80 @@ class UserProfilePage extends StatelessWidget {
 ***REMOVED***
 ***REMOVED***
         body: ListView(
+      children: [
+        Column(
           children: [
-            Column(
+            Container(
   ***REMOVED***
-                Container(
-      ***REMOVED***
-                      gradient: LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                          colors: [Colors.black, Colors.black54]
-        ***REMOVED***
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [Colors.black, Colors.black54])),
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+    ***REMOVED***
+                mainAxisAlignment: MainAxisAlignment.end,
+    ***REMOVED***
+                  //SizedBox(height: 50),
+                  Text(
+                    name,
+***REMOVED***color: Colors.white, fontSize: 26),
     ***REMOVED***,
-                  height: 400,
-                  width: 400,
+    ***REMOVED***
+                    height: 20,
+    ***REMOVED***,
+    ***REMOVED***
+    ***REMOVED***
         ***REMOVED***
-                    mainAxisAlignment: MainAxisAlignment.end,
-        ***REMOVED***
-                      //Adding a sizebox in place of the profile pick cause i dont know how to add one. Sounds like a you problem
-***REMOVED***height: 50),
-***REMOVED***
-                        name,
-    ***REMOVED***color: Colors.white, fontSize: 26),
-        ***REMOVED***,
-***REMOVED***
-                        height: 20,
-        ***REMOVED***,
-
-        ***REMOVED***
-        ***REMOVED***
-            ***REMOVED***
-                          // CUSTOMERS
-                          Column(children: [
-      ***REMOVED***
-                              "$customers",
-                              style:
-                              TextStyle(color: Colors.white, fontSize: 24),
-              ***REMOVED***,
-      ***REMOVED***
-                              "Students",
-                              style:
-                              TextStyle(color: Colors.white, fontSize: 16),
-              ***REMOVED***,
-                          ]),
-                          // RATINGS
-                          Column(children: [
-              ***REMOVED***children: [
-        ***REMOVED***
-                                "$rating",
-            ***REMOVED***
-            ***REMOVED*** fontSize: 24),
-                ***REMOVED***,
-                              Icon(
-                                Icons.star,
-                                color: Colors.yellow,
-                ***REMOVED***
-                            ]),
-      ***REMOVED***
-                              "Rating",
-                              style:
-                              TextStyle(color: Colors.white, fontSize: 16),
-              ***REMOVED***,
-                          ]),
-                          // YEARS
-                          Column(children: [
-      ***REMOVED***
-                              "$years",
-                              style:
-                              TextStyle(color: Colors.white, fontSize: 24),
-              ***REMOVED***,
-      ***REMOVED***
-                              "Years",
-                              style:
-                              TextStyle(color: Colors.white, fontSize: 16),
-              ***REMOVED***,
-                          ])
-        ***REMOVED***
-        ***REMOVED***,
-***REMOVED***height: 20)
+                      // CUSTOMERS
+                      Column(children: [
+  ***REMOVED***
+                          numClasses.toString(),
+      ***REMOVED***color: Colors.white, fontSize: 24),
+          ***REMOVED***,
+  ***REMOVED***
+                          "Classes Registered",
+      ***REMOVED***color: Colors.white, fontSize: 12),
+          ***REMOVED***,
+                      ]),
+                      // RATINGS
+                      Column(children: [
+          ***REMOVED***children: [
+    ***REMOVED***
+                            numPrivateSessions.toString(),
+        ***REMOVED***color: Colors.white, fontSize: 24),
+            ***REMOVED***,
+                        ]),
+  ***REMOVED***
+                          'Private Sessions',
+      ***REMOVED***color: Colors.white, fontSize: 12),
+          ***REMOVED***,
+                      ]),
+                      // YEARS
+                      Column(children: [
+  ***REMOVED***
+                          numVideos.toString(),
+      ***REMOVED***color: Colors.white, fontSize: 24),
+          ***REMOVED***,
+  ***REMOVED***
+                          "Videos Purchased",
+      ***REMOVED***color: Colors.white, fontSize: 12),
+          ***REMOVED***,
+                      ])
     ***REMOVED***
     ***REMOVED***,
-  ***REMOVED***,
-                Column(
-      ***REMOVED***
-                    Card(
-            ***REMOVED***
-            ***REMOVED***
-                          ListTile(
-                            leading: Icon(Icons.star),
-                            title: Text("Place as many of these as you want"),
-                            trailing: Icon(Icons.edit),
-            ***REMOVED***,
-        ***REMOVED***
-        ***REMOVED***,
-      ***REMOVED***,
-                    Card(
-            ***REMOVED***
-            ***REMOVED***
-                          ListTile(
-                            leading: Icon(Icons.star),
-                            title: Text("Or you can make a class for these"),
-                            trailing: Icon(Icons.edit),
-            ***REMOVED***,
-        ***REMOVED***
-        ***REMOVED***,
-      ***REMOVED***,
-                    Card(
-            ***REMOVED***
-            ***REMOVED***
-                          ListTile(
-                            leading: Icon(Icons.star),
-                            title: Text("Its up to you"),
-                            trailing: Icon(Icons.edit),
-            ***REMOVED***,
-        ***REMOVED***
-        ***REMOVED***,
-      ***REMOVED***,
-                    FlatButton(
-  ***REMOVED***'Log Out'),
-                      onPressed: () async {
-                        await FirebaseAuth.instance.signOut();
-                        Navigator.popAndPushNamed(context, '/register');
-                      ***REMOVED***,
-      ***REMOVED***
-  ***REMOVED***
-  ***REMOVED***,
-  ***REMOVED***
+    ***REMOVED***height: 140)
+***REMOVED***
+***REMOVED***,
 ***REMOVED***
           ],
-        ));
+***REMOVED***
+      ],
+    ));
   ***REMOVED***
 ***REMOVED***
 
-
 //////Here is the page for students
-
 
 class StudentProfilePage extends StatelessWidget {
   int sessions;
@@ -173,141 +113,136 @@ class StudentProfilePage extends StatelessWidget {
 ***REMOVED***
 ***REMOVED***
         appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text("Student Profile"),
+            backgroundColor: Colors.black,
+            title: Text("Student Profile"),
             leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-                onPressed: () {***REMOVED***,
-  ***REMOVED***),
-               body: StreamBuilder(
-                  stream: Firestore.instance.collection('Student Profile Page').snapshots(),
-                        builder: (context,snapshot){
-                    if(!snapshot.hasData) return const Text('Loading...');
-               return ListView(
-         ***REMOVED***
-                      Column(
-             ***REMOVED***
-                           Container(
-              ***REMOVED***
-                             gradient: LinearGradient(
-                               begin: Alignment.topLeft,
-                               end: Alignment.bottomRight,
-                               colors: [Colors.black, Colors.black87]
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {***REMOVED***,
+            )),
+        body: StreamBuilder(
+            stream: Firestore.instance
+                .collection('Student Profile Page')
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return const Text('Loading...');
+              return ListView(
+    ***REMOVED***
+                  Column(
+        ***REMOVED***
+                      Container(
             ***REMOVED***
-       ***REMOVED***,
-                   height: 400,
-                          width: 400,
-             ***REMOVED***
-                         mainAxisAlignment: MainAxisAlignment.end,
+                            gradient: LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [Colors.black, Colors.black87])),
+                        height: 400,
+                        width: 400,
               ***REMOVED***
-    //Adding a sizebox in place of the profile pick cause i dont know how to add one. Sounds like a you problem
-       ***REMOVED***height: 50),
-         ***REMOVED***
-                                  name,
-          ***REMOVED***color: Colors.white, fontSize: 26),
-                 ***REMOVED***,
+                          mainAxisAlignment: MainAxisAlignment.end,
+              ***REMOVED***
+      ***REMOVED***height: 50),
       ***REMOVED***
-                             height: 20,
-                ***REMOVED***,
-
-                ***REMOVED***
-             ***REMOVED***
-                  ***REMOVED***
-    // CUSTOMERS
-                                 Column(children: [
-               ***REMOVED***
-                                      "$sessions",
-                                       style:
-                                 TextStyle(color: Colors.white, fontSize: 24),
-                     ***REMOVED***,
-           ***REMOVED***
-                                  "Sessions",
-                                      style:
-                                    TextStyle(color: Colors.white, fontSize: 16),
-                    ***REMOVED***,
-                                 ]),
-    // RATINGS
-                            Column(children: [
-                   ***REMOVED***children: [
-             ***REMOVED***
-                                    "$rating",
-               ***REMOVED***
-           ***REMOVED*** fontSize: 24),
-                    ***REMOVED***,
-                                Icon(
-                                 Icons.star,
-                                 color: Colors.yellow,
-                 ***REMOVED***
-                               ]),
-      ***REMOVED***
-                               "Rating",
-                                 style:
-                                 TextStyle(color: Colors.white, fontSize: 16),
-                 ***REMOVED***,
-                             ]),
-    // YEARS
-                          Column(children: [
-         ***REMOVED***
-                               "$years",
-                                  style:
-                                 TextStyle(color: Colors.white, fontSize: 24),
-                ***REMOVED***,
-      ***REMOVED***
-                                 "Years",
-                                  style:
-                               TextStyle(color: Colors.white, fontSize: 16),
+                              name,
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 26),
               ***REMOVED***,
-                           ])
-          ***REMOVED***
-          ***REMOVED***,
-  ***REMOVED***height: 20)
       ***REMOVED***
-    ***REMOVED***,
-  ***REMOVED***,
-                   Column(
-        ***REMOVED***
-                          Card(
-                ***REMOVED***
+                              height: 20,
+              ***REMOVED***,
               ***REMOVED***
-                            ListTile(
-                               leading: Icon(Icons.star),
-                               title: Text("Place as many of these as you want"),
-                            trailing: Icon(Icons.edit),
-            ***REMOVED***,
-         ***REMOVED***
-       ***REMOVED***,
-    ***REMOVED***,
-                 Card(
-          ***REMOVED***
-           ***REMOVED***
-                         ListTile(
-                            leading: Icon(Icons.star),
-                            title: Text("Or you can make a class for these"),
-                            trailing: Icon(Icons.edit),
-          ***REMOVED***,
-     ***REMOVED***
-    ***REMOVED***,
-***REMOVED***,
-               Card(
-      ***REMOVED***
-       ***REMOVED***
-                         ListTile(
-                           leading: Icon(Icons.star),
-                           title: Text("Its up to you"),
-                           trailing: Icon(Icons.edit),
-                    ***REMOVED***,
-                 ***REMOVED***
-                ***REMOVED***,
+              ***REMOVED***
+                  ***REMOVED***
+                                Column(children: [
             ***REMOVED***
-         ***REMOVED***
+                                    "$sessions",
+                ***REMOVED***
+                ***REMOVED*** fontSize: 24),
+                    ***REMOVED***,
+            ***REMOVED***
+                                    "Sessions",
+                ***REMOVED***
+                ***REMOVED*** fontSize: 16),
+                    ***REMOVED***,
+                                ]),
+                                Column(children: [
+                    ***REMOVED***children: [
+              ***REMOVED***
+                                      "$rating",
+                  ***REMOVED***
+                  ***REMOVED*** fontSize: 24),
+                      ***REMOVED***,
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.yellow,
+                      ***REMOVED***
+                                  ]),
+            ***REMOVED***
+                                    "Rating",
+                ***REMOVED***
+                ***REMOVED*** fontSize: 16),
+                    ***REMOVED***,
+                                ]),
+                                Column(children: [
+            ***REMOVED***
+                                    "$years",
+                ***REMOVED***
+                ***REMOVED*** fontSize: 24),
+                    ***REMOVED***,
+            ***REMOVED***
+                                    "Years",
+                ***REMOVED***
+                ***REMOVED*** fontSize: 16),
+                    ***REMOVED***,
+                                ])
+              ***REMOVED***
+              ***REMOVED***,
+      ***REMOVED***height: 20)
+          ***REMOVED***
+          ***REMOVED***,
+        ***REMOVED***,
+                      Column(
+            ***REMOVED***
+                          Card(
+                  ***REMOVED***
+                  ***REMOVED***
+                                ListTile(
+                                  leading: Icon(Icons.star),
+                                  title: Text(
+                                      "Place as many of these as you want"),
+                                  trailing: Icon(Icons.edit),
+                  ***REMOVED***,
+              ***REMOVED***
+              ***REMOVED***,
+            ***REMOVED***,
+                          Card(
+                  ***REMOVED***
+                  ***REMOVED***
+                                ListTile(
+                                  leading: Icon(Icons.star),
+                                  title:
+                ***REMOVED***"Or you can make a class for these"),
+                                  trailing: Icon(Icons.edit),
+                  ***REMOVED***,
+              ***REMOVED***
+              ***REMOVED***,
+            ***REMOVED***,
+                          Card(
+                  ***REMOVED***
+                  ***REMOVED***
+                                ListTile(
+                                  leading: Icon(Icons.star),
+                                  title: Text("Its up to you"),
+                                  trailing: Icon(Icons.edit),
+                  ***REMOVED***,
+              ***REMOVED***
+              ***REMOVED***,
+            ***REMOVED***
         ***REMOVED***
-   ***REMOVED***
- ***REMOVED***,
- ***REMOVED***
-      ***REMOVED***
-         ***REMOVED***
-       )
-
+        ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***,
 ***REMOVED***
+          ***REMOVED***
+            ***REMOVED***));
   ***REMOVED***
 ***REMOVED***
